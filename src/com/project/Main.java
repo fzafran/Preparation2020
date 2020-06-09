658427e7cdffff4978be4a033a39807c3584c0dd
Farheen <farhan@192.168.1.103>
2020-06-10 00:46:50 +0500

    even odd index

--- src/com/project/Main.java
+++ src/com/project/Main.java
@@ -1,166 +1,111 @@
 package com.project;
 
+
 import java.util.Scanner;
 
 public class Main {
 
 
-        public static void main (String[]args){
-            String str = "MaybNcadmeefigshainjukml";
-            char ch1 = str.charAt(0);
-
-            char ch2 = str.charAt(1);
-
-            char ch3 = str.charAt(2);
-
-            char ch4 = str.charAt(3);
-
-            char ch5 = str.charAt(4);
-
-            char ch6= str.charAt(5);
-
-            char ch7 = str.charAt(6);
-
-            char ch8 = str.charAt(7);
-            char ch9 = str.charAt(8);
-
-            char ch10 = str.charAt(9);
-
-            char ch11 = str.charAt(10);
-
-            char ch12 = str.charAt(11);
-            char ch13 = str.charAt(12);
-
-            char ch14 = str.charAt(13);
-
-            char ch15 = str.charAt(14);
-
-            char ch16 = str.charAt(15);
-            char ch17 = str.charAt(16);
-
-            char ch18 = str.charAt(17);
-
-            char ch19 = str.charAt(18);
-            char ch20 = str.charAt(19);
 
 
-            char ch21 = str.charAt(20);
-
-            char ch22 = str.charAt(22);
-
-            char ch23 = str.charAt(22);
-
-            char ch24 = str.charAt(23);
-
-            System.out.println("Char position : " + ch1 + ch3);
-            System.out.println("Character position: " + ch2 + ch4);
-            System.out.println("Character position " + ch5+ ch7);
-            System.out.println("Character position: " + ch6 + ch8);
-            System.out.println("Character position: " + ch9 + ch11);
-            System.out.println("Character position: " + ch10 + ch12);
-            System.out.println("Character position: " + ch13 + ch15);
-            System.out.println("Character position: " + ch14 + ch16);
-            System.out.println("Character position: " + ch17 + ch19);
-            System.out.println("Character position: " + ch18 + ch20);
-            System.out.println("Character position: " + ch21 + ch23);
-            System.out.println("Character position: " + ch22+ ch24);
+   // private static Scanner scanner = new Scanner(System.in);
 
 
+    public static void main(String[] args) {
+        System.out.println("Enter String:");
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+
+        char[] array = s.toCharArray();
+
+        int count = 0;
+
+        String even = "e";
+        String odd = "0";
+        for (char ch : array) {
 
 
+            if (count % 2 == 0) {
+                even = even + ch;
+            } else {
+                odd = odd + ch;
+            }
+            count++;
         }
+        count = 0;
+
+        System.out.println(even + " " + odd);
     }
+}
 
-
+          //  String str = "MaybNcadmeefigshainjukml";
+//            char ch1 = str.charAt(0);
 //
-//        String randomchar = "MaybNc admeef igshai njukml";
+//            char ch2 = str.charAt(1);
 //
-//        char first = randomchar.charAt(0);
-//        char third = randomchar.charAt(randomchar.length() - 25);
+//            char ch3 = str.charAt(2);
 //
-//        char second = randomchar.charAt(1);
-//        char forth = randomchar.charAt(randomchar.length() - 24);
+//            char ch4 = str.charAt(3);
 //
+//            char ch5 = str.charAt(4);
 //
-//      char fifth = randomchar.charAt(4);
-//       char seventh = randomchar.charAt(randomchar.length() - 26);
+//            char ch6= str.charAt(5);
 //
-//        char sixth = randomchar.charAt(5);
-//         char eighth = randomchar.charAt(randomchar.length() -23);
+//            char ch7 = str.charAt(6);
 //
+//            char ch8 = str.charAt(7);
+//            char ch9 = str.charAt(8);
 //
-//       char nine = randomchar.charAt(6);
-//       char eleven = randomchar.charAt(randomchar.length() -21 );
+//            char ch10 = str.charAt(9);
 //
-//       char ten = randomchar.charAt(7);
-//     char twelve = randomchar.charAt(randomchar.length() - 20);
+//            char ch11 = str.charAt(10);
 //
+//            char ch12 = str.charAt(11);
+//            char ch13 = str.charAt(12);
 //
-//        char thirteen = randomchar.charAt(8);
-//      char fifteen = randomchar.charAt(randomchar.length() - 20);
+//            char ch14 = str.charAt(13);
 //
-//        char fourteen = randomchar.charAt(9);
-//       char sixteen = randomchar.charAt(randomchar.length() - 20);
+//            char ch15 = str.charAt(14);
 //
-//       char seventeen = randomchar.charAt(10);
-//        char nineteen = randomchar.charAt(randomchar.length() -20);
+//            char ch16 = str.charAt(15);
+//            char ch17 = str.charAt(16);
 //
-//       char eighteen = randomchar.charAt(11);
-//        char twenty = randomchar.charAt(randomchar.length() - 20);
+//            char ch18 = str.charAt(17);
 //
-//       char twentyone = randomchar.charAt(12);
-//      char twentythree = randomchar.charAt(randomchar.length() - 20);
-//
-//        char twentytwo = randomchar.charAt(13);
-//        char twentyfour = randomchar.charAt(randomchar.length() - 20);
+//            char ch19 = str.charAt(18);
+//            char ch20 = str.charAt(19);
 //
 //
+//            char ch21 = str.charAt(20);
 //
-//        System.out.printf( "first character of string \"%s\" is '%c' %n", randomchar, first);
+//            char ch22 = str.charAt(22);
 //
-//        System.out.printf("third character of string \"%s\" is '%c' %n", randomchar, third) ;
-//        System.out.printf("second character of string \"%s\" is  %c' %n", randomchar ,second);
-//        System.out.printf("forth  character of string \"%s\" is '%c' %n " , randomchar, forth);
+//            char ch23 = str.charAt(22);
 //
-//       System.out.printf("  fifth character of string \"%s\" is '%c' %n", randomchar, fifth) ;
-//        System.out.printf(" seventh character of string \"%s\" is '%c' %n", randomchar, seventh) ;
-//        System.out.printf("sixth character of string \"%s\" is  %c' %n", randomchar ,
-//               sixth);
-//       System.out.printf("eight  character of string \"%s\" is '%c' %n " , randomchar, eighth);
+//            char ch24 = str.charAt(23);
 //
-//       System.out.printf("9 character of string \"%s\" is '%c' %n", randomchar, nine) ;
-//       System.out.printf("11 character of string \"%s\" is '%c' %n", randomchar, eleven) ;
-//       System.out.printf("10 character of string \"%s\" is  %c' %n", randomchar ,ten);
-//       System.out.printf("12  character of string \"%s\" is '%c' %n " , randomchar, twelve);
-//
-//       System.out.printf("13 character of string \"%s\" is '%c' %n", randomchar, thirteen) ;
-//       System.out.printf("15 character of string \"%s\" is '%c' %n", randomchar, fifteen) ;
-//        System.out.printf("14 character of string \"%s\" is  %c' %n", randomchar ,fourteen);
-//        System.out.printf("16  character of string \"%s\" is '%c' %n " , randomchar, sixteen);
-//
-//      System.out.printf("17 character of string \"%s\" is '%c' %n", randomchar, seventeen) ;
-//        System.out.printf("19 character of string \"%s\" is '%c' %n", randomchar, nineteen) ;
-//       System.out.printf("18 character of string \"%s\" is  %c' %n", randomchar ,eighteen);
-//        System.out.printf("20  character of string \"%s\" is '%c' %n " , randomchar, twenty);
-//
-//
-//       System.out.printf("21 character of string \"%s\" is '%c' %n", randomchar,
-//                twentyone) ;
-//      System.out.printf("23 character of string \"%s\" is '%c' %n", randomchar, twentythree) ;
-//      System.out.printf("22 character of string \"%s\" is  %c' %n", randomchar ,
-//                twentytwo);
-//       System.out.printf("24  character of string \"%s\" is '%c' %n " , randomchar, twentyfour);
+//            System.out.println("Char position : " + ch1 + ch3);
+//            System.out.println("Character position: " + ch2 + ch4);
+//            System.out.println("Character position " + ch5+ ch7);
+//            System.out.println("Character position: " + ch6 + ch8);
+//            System.out.println("Character position: " + ch9 + ch11);
+//            System.out.println("Character position: " + ch10 + ch12);
+//            System.out.println("Character position: " + ch13 + ch15);
+//            System.out.println("Character position: " + ch14 + ch16);
+//            System.out.println("Character position: " + ch17 + ch19);
+//            System.out.println("Character position: " + ch18 + ch20);
+//            System.out.println("Character position: " + ch21 + ch23);
+//            System.out.println("Character position: " + ch22+ ch24);
 //
 //
 //
+//
+//        }
 //    }
-//}
-//
 
 
 
-//end task
-//rnadomchar
 
 // write your code here
 //        System.out.println("Hello World");
